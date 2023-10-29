fea64c9cd420d17a354494b4009bc1db9b9bd9e1
Kurnia Ramadhan Putra <kurniaramadhan@itenas.ac.id>
2023-10-29 22:32:15 +0700

    Build microservices Loans & Cards

---
+++ Part2/loans/src/main/java/com/jvmitenas/loans/LoansApplication.java
@@ -0,0 +1,41 @@
+package com.jvmitenas.loans;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+
+@SpringBootApplication
+@EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+@OpenAPIDefinition(
+	info = @Info(
+		title = "Loans microservice REST API Documentation",
+		description = "ItenasBank Loans microservice REST API Documentation",
+		version = "v1",
+		contact = @Contact(
+			name = "Kurnia Ramadhan Putra",
+			email = "instructor@jvmitenas.com",
+			url = "https://www.linkedin.com/in/kurniaramadhanputra/"
+		),
+		license = @License(
+			name = "Apache 2.0",
+			url = "https://www.jvmitenas.com"
+		)
+	),
+	externalDocs = @ExternalDocumentation(
+		description = "ItenasBank Loans microservice REST API Documentation",
+		url = "https://www.jvmitenas.com/swagger-ui.html"
+	)
+)
+public class LoansApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoansApplication.class, args);
+	}
+
+}
